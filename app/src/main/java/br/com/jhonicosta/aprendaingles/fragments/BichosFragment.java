d8163a342565df90bb345b5344ecc3388e339b6c
jhoni-costa <jhoni_costa@outlook.com>
2018-10-03 16:20:08 -0300

    Executando sons Activity Bichos

--- app/src/main/java/br/com/jhonicosta/aprendaingles/fragments/BichosFragment.java
+++ app/src/main/java/br/com/jhonicosta/aprendaingles/fragments/BichosFragment.java
@@ -1,5 +1,6 @@
 package br.com.jhonicosta.aprendaingles.fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,9 +11,10 @@ import android.widget.ImageButton;
 
 import br.com.jhonicosta.aprendaingles.R;
 
-public class BichosFragment extends Fragment {
+public class BichosFragment extends Fragment implements View.OnClickListener {
 
     private ImageButton cao, gato, leao, macaco, ovelha, vaca;
+    private MediaPlayer mediaPlayer;
 
     public BichosFragment() {
     }
@@ -23,8 +25,59 @@ public class BichosFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_bichos, container, false);
 
         cao = view.findViewById(R.id.cao);
+        gato = view.findViewById(R.id.gato);
+        leao = view.findViewById(R.id.leao);
+        macaco = view.findViewById(R.id.macaco);
+        ovelha = view.findViewById(R.id.ovelha);
+        vaca = view.findViewById(R.id.vaca);
+
+        cao.setOnClickListener(this);
+        gato.setOnClickListener(this);
+        leao.setOnClickListener(this);
+        macaco.setOnClickListener(this);
+        ovelha.setOnClickListener(this);
+        vaca.setOnClickListener(this);
+
 
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cao:
+                tocarSom(R.raw.dog);
+                break;
+            case R.id.gato:
+                tocarSom(R.raw.cat);
+                break;
+            case R.id.leao:
+                tocarSom(R.raw.lion);
+                break;
+            case R.id.macaco:
+                tocarSom(R.raw.monkey);
+                break;
+            case R.id.ovelha:
+                tocarSom(R.raw.sheep);
+                break;
+            case R.id.vaca:
+                tocarSom(R.raw.cow);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void tocarSom(int som) {
+        mediaPlayer = MediaPlayer.create(getActivity(), som);
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
 }
--- app/src/main/java/br/com/jhonicosta/aprendaingles/fragments/VogaisFragment.java
+++ app/src/main/java/br/com/jhonicosta/aprendaingles/fragments/VogaisFragment.java
@@ -5,10 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import br.com.jhonicosta.aprendaingles.R;
 
 public class VogaisFragment extends Fragment {
+
+    private ImageButton um, dois, tres, quatro, cinco, seis;
 
     public VogaisFragment() {
     }
@@ -16,7 +19,9 @@ public class VogaisFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_vogais, container, false);
+        View view =  inflater.inflate(R.layout.fragment_vogais, container, false);
+
+        return view;
     }
 
 }
